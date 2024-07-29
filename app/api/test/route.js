d2c5ff11691dee05a9a31d2a8c3612ffd938251f
WGMI <imo.muganda21@gmail.com>
2024-07-29 16:41:33 +0300

    Ping server

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
     try {
-        const res = await axios.get("http://209.38.210.50/test");
+        const res = await axios.get("http://159.223.137.5/test");
         return new NextResponse("Successfully pinged", { status: 200 });
     } catch (error) {
         return new NextResponse("Failed: " + error, { status: 500 });
